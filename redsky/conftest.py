255b3e0945b6f08e7406af35c7ffb49bd1ec7526
Mohammed Y. Alnajdi <mohdnagfy@gmail.com>
2025-03-03 03:34:39 +0300

    inital commit

---
+++ redsky/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from redsky.users.models import User
+from redsky.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
